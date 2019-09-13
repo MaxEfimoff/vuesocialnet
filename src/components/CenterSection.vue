f1a6071d7e7621a03483bba9301e5f40ae8cff70
Max Efimoff <emv3@ya.ru>
2019-09-13 13:19:47 +0400

    updated central section

--- src/components/CenterSection.vue
+++ src/components/CenterSection.vue
@@ -1,79 +1,80 @@
 <template>
   <section class="section-center">
-          <div class="section posts">
-            <div class="friends-list padding">
-              <ul  class="flex">
-                <li>
-                  <a href="">All posts</a>
-                </li>
-                <li>
-                  <a href="">My posts</a>
-                </li>
-                <li>
-                  <a href="">Friends posts</a>
-                </li>
-                <li>
-                  <a href="">Recommended</a>
-                </li>
-                <li>
-                  <a href="">Liked</a>
-                </li>
-              </ul>
-              <form action="#" class="post-new-form">
-                <input type="text" placeholder="Posts search" />
-              </form>
-            </div>
-              <div class="post">
-                <div class="post-author">
-                  <div class="groups-photo">
-                    <img class="groups-img" src="../assets/img/groups/01.jpg" alt="">
-                    <span>Formula 1 </span>
-                  </div>
-                </div>
-                <div class="post-text">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quasi rerum, illo velit culpa sunt neque suscipit, eos voluptatibus consectetur corrupti maxime eaque tempora. Laboriosam perspiciatis dicta obcaecati ad aliquid aspernatur magni. Suscipit, neque esse optio ab facere sunt unde!
-                </div>
-              </div>
-              <div class="post">
-                <div class="post-author">
-                  <div class="groups-photo">
-                    <img class="groups-img" src="../assets/img/groups/02.jpg" alt="">
-                    <span>Formula 1 </span>
-                  </div>
-                </div>
-                <div class="post-text">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quasi rerum, illo velit culpa sunt neque suscipit, eos voluptatibus consectetur corrupti maxime eaque tempora. Laboriosam perspiciatis dicta obcaecati ad aliquid aspernatur magni. Suscipit, neque esse optio ab facere sunt unde!
-                </div>
-              </div>
-              <div class="post">
-                <div class="post-author">
-                  <div class="groups-photo">
-                    <img class="groups-img" src="../assets/img/groups/03.jpg" alt="">
-                    <span>Formula 1 </span>
-                  </div>
-                </div>
-                <div class="post-text">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis alias reiciendis enim explicabo facere sapiente numquam animi excepturi, sunt cum. Porro, dolores laborum harum vero adipisci similique earum voluptas expedita.
-                </div>
-              </div>
-              <div class="post">
-                <div class="post-author">
-                  <div class="groups-photo">
-                    <img class="groups-img" src="../assets/img/groups/04.jpg" alt="">
-                    <span>Formula 1 </span>
-                  </div>
-                </div>
-                <div class="post-text">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit reprehenderit beatae ducimus. Odio sequi asperiores similique? Modi asperiores, corporis est impedit eveniet porro quia aut voluptatum hic laborum ducimus rerum repellat, iure nobis sunt tempora natus tempore culpa voluptatibus quaerat rem blanditiis fugit id? Odio ut culpa incidunt cum ex?
-                </div>
-              </div>
-            </div>
-      </section>
+    <div class="section posts">
+      <div class="friends-list padding">
+        <ul  class="flex">
+          <li v-for="item in topMenu" :key="item.name">
+            <a href="">{{ item.name }}</a>
+          </li>
+        </ul>
+        <form action="#" class="post-new-form">
+          <input type="text" placeholder="Posts search" />
+        </form>
+      </div>
+      <div class="post" v-for="post in postsList" :key="post.id">
+        <div class="post-author">
+          <div class="groups-photo">
+            <img class="groups-img" :src=" require(`@/assets/img/groups/${post.url}`) " alt="">
+            <span>{{ post.groupName }}</span>
+          </div>
+        </div>
+        <div class="post-text">
+          {{ post.content }}
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      topMenu: [
+        {
+          name: "All Posts"
+        },
+        {
+          name: "My Posts"
+        },
+        {
+          name: "Friends Posts"
+        },
+        {
+          name: "Recommended"
+        },
+        {
+          name: "Liked"
+        },
+      ],
+      postsList: [
+        {
+          id: 1,
+          url: "03.jpg",
+          groupName: "Formula 1",
+          content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quasi rerum, illo velit culpa sunt neque suscipit, eos voluptatibus consectetur corrupti maxime eaque tempora. Laboriosam perspiciatis dicta obcaecati ad aliquid aspernatur magni. Suscipit, neque esse optio ab facere sunt unde!"
+        },
+        {
+          id: 2,
+          url: "01.jpg",
+          groupName: "Garden design",
+          content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quasi rerum, illo velit culpa sunt neque suscipit, eos voluptatibus consectetur corrupti maxime eaque tempora."
+        },
+        {
+          id: 3,
+          url: "02.jpg",
+          groupName: "Cafe racer",
+          content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quasi rerum, illo velit culpa sunt neque suscipit, eos voluptatibus consectetur corrupti maxime eaque tempora. Laboriosam perspiciatis dicta obcaecati ad aliquid aspernatur magni. Suscipit"
+        },
+        {
+          id: 4,
+          url: "04.jpg",
+          groupName: "Odlo",
+          content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quasi rerum, illo velit culpa sunt neque suscipit, eos voluptatibus consectetur corrupti maxime eaque tempora. Laboriosam perspiciatis dicta obcaecati ad aliquid aspernatur magni. Suscipit, neque esse optio ab facere sunt unde!"
+        },
+      ]
+    }
+  }
 }
 </script>
 
