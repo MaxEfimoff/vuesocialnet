7ef568928ccd1c9da589e88e7f5b365b188701e3
Max Efimoff <emv3@ya.ru>
2019-10-03 22:50:31 +0400

    removed dev keys from gitignore

---
+++ back/config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb://wildblood:erager164572@cluster0-shard-00-00-bcuop.mongodb.net:27017,cluster0-shard-00-01-bcuop.mongodb.net:27017,cluster0-shard-00-02-bcuop.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",
+  secretOrKey: "secret"
+};
